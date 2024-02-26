7e15b535264bd7c15ee91b7da468588647a0211d
akmindaugas <mindaugas.akelis@gmail.com>
2024-02-26 18:51:40 +0200

    manual add on

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+var emailInput = document.getElementById('user-email');
+var passwordInput = document.getElementById('user-password');
+var mainButton = document.getElementById('main-btn');
+
+mainButton.addEventListener('DOMContentLoaded', function () {
+    console.log('Button was clicked');
+    var userLoginData = {
+        email: emailInput.value,
+        password: passwordInput.value
+    };
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // sauktukas pakeicia verte ir true i false: codas vykdomas jeigu reiksme yra bloga, false. priesingu atveju cokas nebus paleidziamas
+    if (!emailRegex.test(userLoginData.email) === false) {
+        console.log('email is not valid');
+        return;
+    }
+    ;
+    if (userLoginData.password.length <= 8) {
+        console.log('password must be 8+ characters long');
+        return;
+    }
+    ;
+    console.log('all good');
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,54 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+
+// // const nameInput1: HTMLElement | null = document.getElementById('user-name');  // pirmas sperndimas, kaip sutvarkyti null
+
+// const nameInput2: HTMLElement = document.getElementById('user-name')!; // antras sprendimas, kaip sutvarkyti null
+
+
+// const nameInput3: HTMLElement = document.getElementById('user-name') as HTMLElement; // trecias sprendimas, kaip sutvarkyti null
+
+// const nameInput4: HTMLInputElement = document.getElementById('user-name') as HTMLInputElement; // ketvirtas sprendimas, kaip sutvarkyti null
+
+
+// //==============================================================
+
+// const nameInput5: HTMLInputElement = document.querySelector('input[id="user-name"]')!; // penktas sprendimas, kaip sutvarkyti null
+
+// console.log(nameInput5);
+
+// const button: HTMLElement = document.getElementById('main-btn')!;
+// const button2: HTMLElement = document.querySelector('button[id="main-btn"]')!;
+
+
+type UserLogin ={
+    email: string;
+    password: string;
+};
+
+const emailInput: HTMLInputElement = document.getElementById('user-email') as HTMLInputElement;
+const passwordInput: HTMLInputElement = document.getElementById('user-password') as HTMLInputElement;
+const mainButton: HTMLButtonElement = document.getElementById('main-btn') as HTMLButtonElement;
+
+mainButton.addEventListener('click', () => {
+    console.log('Button was clicked');
+    
+    const userLoginData = {
+        email: emailInput.value,
+        password: passwordInput.value
+    };
+
+    const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// sauktukas pakeicia verte ir true i false: codas vykdomas jeigu reiksme yra bloga, false. priesingu atveju cokas nebus paleidziamas
+    if ( !emailRegex.test(userLoginData.email) === false){
+        console.log('email is not valid');
+        return;
+    };
+if (userLoginData.password.length <=8){
+    console.log('password must be 8+ characters long');
+    return;
+};
+console.log('all good')
+
+
+
+    });
